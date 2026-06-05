5917dc46dbe77ff0b7e2862da836dabb7510d3ff
minhtrifit <minhtri.fit@gmail.com>
2026-06-06 06:11:32 +0700

    feat: update json preview modal

--- src/components/ui/JsonEditor/PreviewModal.tsx
+++ src/components/ui/JsonEditor/PreviewModal.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { Modal } from 'antd';
 import { JsonValue } from './types';
 
@@ -5,14 +6,17 @@ interface PropType {
   jsonContent: JsonValue;
   open: boolean;
   onClose: () => void;
+  className?: string;
 }
 
 const PreviewModal = (props: PropType) => {
-  const { jsonContent, open, onClose } = props;
+  const { jsonContent, open, onClose, className } = props;
 
   return (
     <Modal open={open} footer={null} onCancel={onClose}>
-      <pre>{JSON.stringify(jsonContent, null, 2)}</pre>
+      <div className={cn(className, `mt-8 max-h-[400px] overflow-y-auto`)}>
+        <pre>{JSON.stringify(jsonContent, null, 2)}</pre>
+      </div>
     </Modal>
   );
 };
