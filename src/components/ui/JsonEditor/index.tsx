e8344297b5e40b6a5981aac845e61974b3cd2ee3
minhtrifit <minhtri.fit@gmail.com>
2026-06-06 05:59:56 +0700

    feat: update json editor

--- src/components/ui/JsonEditor/index.tsx
+++ src/components/ui/JsonEditor/index.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-import { forwardRef, useState } from 'react';
+import { forwardRef, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
-import { Button, Tooltip } from 'antd';
-import { FaEye } from 'react-icons/fa';
+import { Button, message, Popconfirm, Tooltip, Upload } from 'antd';
+import { RcFile } from 'antd/es/upload';
+import { FaEye, FaTrash } from 'react-icons/fa';
+import { IoMdDownload } from 'react-icons/io';
+import { MdFileUpload } from 'react-icons/md';
 import { JsonValue } from './types';
 import { JsonNode } from './JsonNode';
 import PreviewModal from './PreviewModal';
+import { downloadJsonFile, isValidJson, readJsonFile } from './utils';
 
 interface PropType {
   containerClassName?: string;
@@ -14,14 +18,34 @@ interface PropType {
   value: JsonValue;
   onChange: (value: JsonValue) => void;
   error?: string;
+  enablePreview?: boolean;
+  enableDownload?: boolean;
+  enableUpload?: boolean;
+  enableClear?: boolean;
 }
 
 export const JsonEditor = forwardRef<HTMLDivElement, PropType>((props, ref) => {
-  const { containerClassName, toolBarClassName, value = {}, onChange, error } = props;
+  const {
+    containerClassName,
+    toolBarClassName,
+    value = {},
+    onChange,
+    error,
+    enablePreview = true,
+    enableDownload = true,
+    enableUpload = true,
+    enableClear = true,
+  } = props;
 
   const { t } = useTranslation();
 
   const [openPreview, setOpenPreview] = useState<boolean>(false);
+  const [uploadLoading, setUploadLoading] = useState<boolean>(false);
+
+  const memoValidJsonValue: boolean = useMemo(() => {
+    if (!value) return false;
+    return isValidJson(JSON.stringify(value, null, 2));
+  }, [value]);
 
   const handlePreview = () => {
     setOpenPreview(true);
@@ -31,6 +55,42 @@ export const JsonEditor = forwardRef<HTMLDivElement, PropType>((props, ref) => {
     setOpenPreview(false);
   };
 
+  const handleDownloadFile = (data: JsonValue) => {
+    const stringData = JSON.stringify(data, null, 2);
+
+    const checkValidJson = isValidJson(stringData);
+
+    if (!checkValidJson) {
+      message.error(t('antd-json-editor.invalid-json'));
+      return;
+    }
+
+    downloadJsonFile(stringData, 'data');
+    message.success(t('antd-json-editor.download-successfully'));
+  };
+
+  const handleUploadFile = async (file: RcFile) => {
+    try {
+      setUploadLoading(true);
+
+      const data: JsonValue = await readJsonFile(file);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      onChange(data);
+      message.success(t('antd-json-editor.upload-successfully'));
+    } catch {
+      message.error(t('antd-json-editor.invalid-json'));
+    } finally {
+      setUploadLoading(false);
+    }
+
+    return false; // not upload to server
+  };
+
+  const handleDeleteAllNode = () => {
+    onChange({});
+  };
+
   return (
     <div
       ref={ref}
@@ -38,7 +98,7 @@ export const JsonEditor = forwardRef<HTMLDivElement, PropType>((props, ref) => {
       className={cn(
         containerClassName,
         error && 'border border-solid border-red-500',
-        'rounded-sm bg-[#FFF] flex flex-col',
+        'w-full rounded-sm bg-[#FFF] flex flex-col',
       )}
     >
       <PreviewModal jsonContent={value} open={openPreview} onClose={handleClosePreview} />
@@ -48,14 +108,57 @@ export const JsonEditor = forwardRef<HTMLDivElement, PropType>((props, ref) => {
         className={cn(
           toolBarClassName,
           error ? 'bg-red-500' : 'bg-primary',
-          'w-full p-2 text-[#FFF] rounded-t-sm',
+          'w-full p-2 text-[#FFF] rounded-t-sm flex flex-wrap items-center gap-2',
         )}
       >
-        <Tooltip title={t('antd-json-editor.preview')}>
-          <Button onClick={handlePreview}>
-            <FaEye size={15} className='text-primary' />
-          </Button>
-        </Tooltip>
+        {enablePreview && (
+          <Tooltip title={t('antd-json-editor.preview')}>
+            <Button onClick={handlePreview}>
+              <FaEye size={20} className='text-primary' />
+            </Button>
+          </Tooltip>
+        )}
+
+        {enableDownload && (
+          <Tooltip title={t('antd-json-editor.download')}>
+            <Button
+              disabled={!memoValidJsonValue}
+              className={cn(`${!memoValidJsonValue && '!bg-zinc-200'}`)}
+              onClick={() => handleDownloadFile(value)}
+            >
+              <IoMdDownload
+                size={20}
+                className={cn(`${!memoValidJsonValue ? 'text-zinc-400' : 'text-primary'}`)}
+              />
+            </Button>
+          </Tooltip>
+        )}
+
+        {enableUpload && (
+          <Tooltip title={t('antd-json-editor.upload')}>
+            <Upload accept='.json' showUploadList={false} beforeUpload={handleUploadFile}>
+              <Button loading={uploadLoading}>
+                <MdFileUpload size={20} className='text-primary' />
+              </Button>
+            </Upload>
+          </Tooltip>
+        )}
+
+        {enableClear && (
+          <Tooltip title={t('antd-json-editor.clear-all')}>
+            <Popconfirm
+              title={t('antd-json-editor.confirm')}
+              description={t('antd-json-editor.confirm-delete-all-node')}
+              onConfirm={handleDeleteAllNode}
+              okText={t('antd-json-editor.confirm')}
+              cancelText={t('antd-json-editor.cancel')}
+            >
+              <Button danger>
+                <FaTrash size={20} />
+              </Button>
+            </Popconfirm>
+          </Tooltip>
+        )}
       </section>
 
       <section className='p-4'>
--- src/components/ui/JsonEditor/utils.ts
+++ src/components/ui/JsonEditor/utils.ts
@@ -36,3 +36,45 @@ export const defaultValueByType = (type: string): JsonValue => {
       return '';
   }
 };
+
+export const isValidJson = (value: string): boolean => {
+  try {
+    JSON.parse(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const downloadJsonFile = (jsonString: string, fileName: string) => {
+  const fileExt = '.json';
+  const downloadName = `${fileName}${fileExt}`;
+
+  const blob = new Blob([jsonString], {
+    type: 'application/json;charset=utf-8',
+  });
+
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = downloadName;
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
+
+export const readJsonFile = async <T = any>(file: File): Promise<T> => {
+  if (!file.name.endsWith('.json')) throw new Error('Please choose json file');
+
+  const text = await file.text();
+
+  try {
+    return JSON.parse(text) as T;
+  } catch {
+    throw new Error('Invalid json');
+  }
+};
